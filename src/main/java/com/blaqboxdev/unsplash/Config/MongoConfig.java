8be1949ff24c21ec70bb8da5c2ae8726596992c2
Karabo Sambo <karabosambo@outlook.com>
2023-09-02 15:23:51 +0200

    Updated mongoDB Connections

---
+++ src/main/java/com/blaqboxdev/unsplash/Config/MongoConfig.java
@@ -0,0 +1,9 @@
+package com.blaqboxdev.unsplash.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+@Configuration
+public class MongoConfig {
+}
